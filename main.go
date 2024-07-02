26e32df8bc19625e6ce96dea32485762c350104e
Rafael Amandio <rafael.amandio@gmail.com>
2024-07-02 13:01:54 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/rafaamandio/gopportunites/router"
+import (
+	"github.com/rafaamandio/gopportunites/config"
+	"github.com/rafaamandio/gopportunites/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization erro: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
